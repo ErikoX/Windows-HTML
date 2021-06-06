25bee05efa4efd7b76f9f507c9658f8a1e5e277a
ErikoX <ponotone@gmail.com>
2021-06-06 11:21:35 +0200

    Beginning Javascript Loader

--- files/js/loader.js
+++ files/js/loader.js
@@ -1,1 +1,17 @@
-console.log("Loading Scripts...")
+console.log("Loading Scripts...");
+
+function addScript(file) {
+    var script = document.getElementById("script");
+    script.src = "files/js/";
+    document.body.appendChild(script);
+}
+
+
+function getScript() {
+    var url = window.location.pathname;
+    var spliturl = url.split("/")
+    var call = spliturl[-1].split(".")[0]
+    addScript(call)
+}
+
+getScript()
