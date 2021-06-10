6220711812355dfc69d9bcc24b86b2ca2d61933e
ErikoX <ponotone@gmail.com>
2021-06-10 16:38:44 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,12 @@
+echo "Starting Website..."
+
 git pull origin master
+
+echo ""
+echo "---------------------------------"
+echo "Pulled newest version from github"
+echo "Great day!"
+echo "---------------------------------"
+echo ""
+
 php -S 0.0.0.0:8000 -t .
